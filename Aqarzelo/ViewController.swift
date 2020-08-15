269290048681ef56e225ca95b029d8c57052b846
hosam mohamed <hosamelmalt@gmail.com>
2020-08-15 13:25:37 -0700

    Initial Commit

---
+++ Aqarzelo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aqarzelo
+//
+//  Created by Hossam on 8/15/20.
+//  Copyright © 2020 Hossam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
