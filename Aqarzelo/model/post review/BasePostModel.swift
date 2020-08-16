ea9d671e14c9e822f65d362ec58e4cf47e2b4dc1
hosam mohamed <hosamelmalt@gmail.com>
2020-08-16 09:17:31 -0700

    done

---
+++ Aqarzelo/model/post review/BasePostModel.swift
@@ -0,0 +1,23 @@
+//
+//  BasePostModel.swift
+//  Aqarzelo
+//
+//  Created by Hossam on 8/15/20.
+//  Copyright © 2020 Hossam. All rights reserved.
+//
+
+import Foundation
+
+struct BasePostModel:Codable {
+    
+    let status: Int
+    let messageEn, messageAr: String
+    var data: [AqarModel]?
+    
+    enum CodingKeys: String, CodingKey {
+        case status
+        case messageEn = "message_en"
+        case messageAr = "message_ar"
+        case data
+    }
+}
